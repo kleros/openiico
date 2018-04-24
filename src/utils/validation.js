c362b6c7f03330daca9eb1cec347d86736bbf06b
epiqueras <epiquerass@gmail.com>
2018-04-24 16:00:07 -0700

    feat(bids): validate form inputs

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,4 +1,11 @@
+// General
 export const required = name => v =>
   v === undefined ? `${name} is required.` : undefined
+
+// Number
 export const number = name => v =>
   Number.isNaN(Number(v)) ? `${name} must be a number.` : undefined
+export const positive = name => v =>
+  Number(v) < 0 ? `${name} must be a positive number.` : undefined
+export const nonZero = name => v =>
+  Number(v) === 0 ? `${name} can't be zero.` : undefined
