e531027f6acb13f402a467db71ae72dc6d34d2d1
epiqueras <epiquerass@gmail.com>
2018-05-10 16:02:25 -0300

    test: set up new integration tests

---
+++ src/containers/home/home.test.js
@@ -0,0 +1,16 @@
+import setupIntegrationTest from '../../bootstrap/setup-integration-test'
+
+let integration = {
+  store: null,
+  history: null,
+  dispatchSpy: null,
+  mountApp: null
+}
+
+beforeEach(() => {
+  integration = setupIntegrationTest({ router: { location: '/' } })
+})
+
+it('Renders.', async () => {
+  integration.mountApp()
+})
