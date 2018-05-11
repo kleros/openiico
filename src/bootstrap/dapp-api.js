cffc0d386bcc0bd1584aa7017daff0d1291f6b44
epiqueras <epiquerass@gmail.com>
2018-05-11 11:32:28 -0500

    test: seed ganache and allow more snapshots

--- src/bootstrap/dapp-api.js
+++ src/bootstrap/dapp-api.js
@@ -7,7 +7,7 @@ const ETHEREUM_PROVIDER = process.env[`REACT_APP_${env}_ETHEREUM_PROVIDER`]
 
 let eth
 if (process.env.NODE_ENV === 'test')
-  eth = new Eth(require('ganache-cli').provider())
+  eth = new Eth(require('ganache-cli').provider({ seed: 1 }))
 else if (window.web3 && window.web3.currentProvider)
   eth = new Eth(window.web3.currentProvider)
 else eth = new Eth(new Eth.HttpProvider(ETHEREUM_PROVIDER))
--- src/containers/home/home.test.js
+++ src/containers/home/home.test.js
@@ -1,5 +1,3 @@
-import toJson from 'enzyme-to-json'
-
 import setupIntegrationTest, {
   flushPromises
 } from '../../bootstrap/setup-integration-test'
@@ -22,22 +20,16 @@ it("Renders and loads an IICO contract's data after submitting an address.", asy
   // Initial render
   const app = integration.mountApp()
   await flushPromises(app)
-  expect(toJson(app.find(Home))).toMatchSnapshot()
+  expect(app.find(Home)).toMatchSnapshot()
 
   // Enter an address
   app.find('.TextInput-input').simulate('change', {
-    target: { value: 'Enter an address' }
-  })
-  expect(toJson(app.find(Home))).toMatchSnapshot()
-  app.find('.TextInput-input').simulate('change', {
     target: { value: integration.contracts.IICO.address }
   })
+  expect(app.find(Home)).toMatchSnapshot()
 
   // Submit address
   app.find('.Home').simulate('keypress', { key: 'Enter' })
   await flushPromises(app)
-  app.find('.TextInput-input').simulate('change', {
-    target: { value: 'Submit address' }
-  })
-  expect(app.find(Home).text()).toMatchSnapshot()
+  expect(app.find(Home)).toMatchSnapshot()
 })
