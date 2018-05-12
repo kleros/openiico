4d8eda881b750d03430a6bba84ffbd6241bfbd91
epiqueras <epiquerass@gmail.com>
2018-05-11 19:49:20 -0500

    feat(chain-number): new significant digit logic

--- src/components/chain-number/index.js
+++ src/components/chain-number/index.js
@@ -4,10 +4,10 @@ import PropTypes from 'prop-types'
 import './chain-number.css'
 
 const ChainNumber = ({ children }) => {
-  const n = children.toFixed(2)
+  const n = children.toFixed(children < 1 ? 6 : 2)
   return (
     <span data-tip={children} className="ChainNumber">
-      {n.length > 5 ? `${n.slice(0, 5)}...` : n}
+      {n.length > 8 ? `${n.slice(0, 8)}...` : n}
     </span>
   )
 }
